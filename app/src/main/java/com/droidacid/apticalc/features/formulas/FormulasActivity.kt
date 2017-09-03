cafd64b8fce1f981f0f3407eb40f54a5083cbeb3
shivamdev31 <makewaysforshivam@gmail.com>
2017-09-03 23:56:59 +0530

    Added dummy activities

---
+++ app/src/main/java/com/droidacid/apticalc/features/formulas/FormulasActivity.kt
@@ -0,0 +1,8 @@
+package com.droidacid.apticalc.features.formulas
+
+/**
+ * Created by shivam on 3/9/17.
+ */
+class FormulasActivity {
+
+}
---
+++ app/src/main/java/com/droidacid/apticalc/features/home/HomeActivity.kt
@@ -0,0 +1,8 @@
+package com.droidacid.apticalc.features.home
+
+/**
+ * Created by shivam on 3/9/17.
+ */
+class HomeActivity {
+
+}
